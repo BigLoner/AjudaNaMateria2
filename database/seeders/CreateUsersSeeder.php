c7e7a62b384182ad912c9998c1dbc0dfa69f9047
Martim Silva <mkii3962005@gmail.com>
2023-03-17 15:01:46 +0000

    Logon/Register consertado(Fazer o Request)

---
+++ database/seeders/CreateUsersSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Moderadores;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CreateUsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       $users =[
+        [
+        'name'=> 'moderador',
+        'email'=> 'moderador@gmail.com',
+        'type'=> '1',
+        'password'=> ('moderador123'),
+
+
+        ],
+        [
+            'name'=> 'professor',
+            'email'=> 'professor@gmail.com',
+            'type'=> '1',
+            'password'=> ('professor123'),
+
+
+            ],
+    ];
+    }
+}
